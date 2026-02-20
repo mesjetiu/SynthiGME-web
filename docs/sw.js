d03e1f0934c81e953ac93b85ef672494cc5f45ac
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2026-02-20 08:40:14 +0100

    release: Synthi GME v0.7.0

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '0.6.0-20260220.080105';
+const CACHE_VERSION = '0.7.0-20260220.084014';
 const CACHE_NAME = `synthigme-cache-v${CACHE_VERSION}`;
 const CORE_ASSETS = [
   './',

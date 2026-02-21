dffe88f401940927e286d451b0b69d3858a11ebb
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2026-02-21 09:29:09 +0100

    build: regenerar docs/

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '0.7.0-20260221.090804';
+const CACHE_VERSION = '0.7.0-20260221.092709';
 const CACHE_NAME = `synthigme-cache-v${CACHE_VERSION}`;
 const CORE_ASSETS = [
   './',

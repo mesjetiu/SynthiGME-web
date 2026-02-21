03bb81c43404b9f2d53ee8b06b28aee00168912c
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2026-02-21 18:29:46 +0100

    fix(splash): 2 segúndso de duración mínima del splash screen

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '0.7.0-20260221.182602';
+const CACHE_VERSION = '0.7.0-20260221.182953';
 const CACHE_NAME = `synthigme-cache-v${CACHE_VERSION}`;
 const CORE_ASSETS = [
   './',

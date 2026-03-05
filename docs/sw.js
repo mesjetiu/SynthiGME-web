56686259dcdcc784f57ed3c7952b74bea22b8adc
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2026-03-05 17:41:59 +0100

    design(keyboards): Añadido svg a src (modificado previamente)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '0.7.0-20260305.122803';
+const CACHE_VERSION = '0.7.0-20260305.174043';
 const CACHE_NAME = `synthigme-cache-v${CACHE_VERSION}`;
 const CORE_ASSETS = [
   './',
